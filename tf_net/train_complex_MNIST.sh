8ae81560e1e80a08f4d9f93f8a3c1b866a80a7a5
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-06-24 11:23:37 -0600

    Increased max run time to 3 days in the shell script

--- tf_net/train_complex_MNIST.sh
+++ tf_net/train_complex_MNIST.sh
@@ -3,12 +3,12 @@
 #SBATCH -o log_slurm.o%j    # output and error file name (%j expands to jobID)
 #SBATCH -c 48                # cpus per task
 #SBATCH -N 1                # number of nodes you want to run on
-#SBATCH --gres=gpu:2        # request a gpu
+#SBATCH --gres=gpu:8       # request a gpu
 #SBATCH -p gpu              # queue (partition)
-#SBATCH -t 09:00:00         # run time (hh:mm:ss)
+#SBATCH -t 3-00:00:00         # run time (DD-hh:mm:ss)
 
 module load conda
 module load cudnn8.0-cuda11.0/8.0.5.39
-conda init
+conda init bash
 conda activate tf_net_3.10
 python main.py
