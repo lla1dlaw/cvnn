bdb2ca9f1d25ac7757dda1b95c5407f215d3edb6
Liam Laidlaw <liamlaidlaw@cpu116.cm.cluster>
2025-06-24 14:11:25 -0600

    Modified training bash script

--- tf_net/train_complex_MNIST.sh
+++ tf_net/train_complex_MNIST.sh
@@ -3,9 +3,9 @@
 #SBATCH -o log_slurm.o%j    # output and error file name (%j expands to jobID)
 #SBATCH -c 48                # cpus per task
 #SBATCH -N 1                # number of nodes you want to run on
-#SBATCH --gres=gpu:8       # request a gpu
-#SBATCH -p gpu              # queue (partition)
-#SBATCH -t 3-00:00:00         # run time (DD-hh:mm:ss)
+#SBATCH --gres=gpu:4       # request a gpu
+#SBATCH -p gpu			# queue (partition)
+#SBATCH -t 24:00:00         # run time (hh:mm:ss)
 
 module load conda
 module load cudnn8.0-cuda11.0/8.0.5.39
